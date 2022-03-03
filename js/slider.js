f9950bdc864452c66977fcd28c24325fa48136f0
iampedrossb <xpedrostewart@gmail.com>
2022-03-03 12:15:10 -0300

    Mudança na paleta de cores para Mickey Mouse e passagem automática do slider, bem como passagem do slider ao clicar na imagem da coluna

--- js/slider.js
+++ js/slider.js
@@ -1,4 +1,6 @@
 const botoesControle = [... document.getElementsByClassName('botao-controle')];
+const colunaTexto = [... document.querySelectorAll('.coluna .texto')];
+const colunaContainerImagem = [... document.querySelectorAll('.coluna .container-imagem')]
 const linha = document.getElementById('linha');
 
 botoesControle.forEach((botao, index) => {
@@ -8,6 +10,38 @@ botoesControle.forEach((botao, index) => {
 		botao.classList.add('ativo');
 	});
 });
+
+
+colunaContainerImagem.forEach((containerImagem, index) => {
+	containerImagem.addEventListener('click', () => {
+		limpaBotoesAtivos();
+		if (index === colunaContainerImagem.length - 1) {
+			linha.style.transform = `translateX(${0}vw)`;
+			botoesControle[0].classList.add('ativo');
+		} else {
+			linha.style.transform = `translateX(-${(index + 1) * 100}vw)`;
+			botoesControle[index + 1].classList.add('ativo');
+		}
+	});
+});
+
+let intervalIndex = 0;
+let movimentacao = 0;
+setInterval(() => {
+	limpaBotoesAtivos();
+	if (intervalIndex === botoesControle.length - 1) {
+		linha.style.transform = `translateX(${0}vw)`;
+		botoesControle[0].classList.add('ativo');
+		intervalIndex = 0;
+		movimentacao = 0;
+
+	} else {
+		movimentacao += 100;
+		linha.style.transform = `translateX(-${movimentacao}vw)`;
+		intervalIndex++;
+		botoesControle[intervalIndex].classList.add('ativo');
+	}
+}, 3000);
 
 function limpaBotoesAtivos() {
 	botoesControle.forEach(botao => {
